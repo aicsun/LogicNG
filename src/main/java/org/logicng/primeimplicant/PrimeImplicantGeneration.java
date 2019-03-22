4e826897a525ffb003a31f35f286953b7e6f3d05
Rouven Walter <rouven.walter@booleworks.com>
2019-03-22 17:03:06 +0100

    prime implicant generation on top of minisat

---
+++ src/main/java/org/logicng/primeimplicant/PrimeImplicantGeneration.java
@@ -0,0 +1,186 @@
+package org.logicng.primeimplicant;
+
+import org.logicng.collections.LNGIntVector;
+import org.logicng.datastructures.Tristate;
+import org.logicng.formulas.Formula;
+import org.logicng.formulas.FormulaFactory;
+import org.logicng.formulas.Literal;
+import org.logicng.formulas.Variable;
+import org.logicng.solvers.datastructures.MSVariable;
+import org.logicng.solvers.sat.MiniSat2Solver;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+/**
+ * Prime implicant generation.
+ *
+ * This class provides methods to generate a prime implicant for a formula.
+ * For a formula, an arbitrary prime implicant can be generated or an already known implicant can be used to extract
+ * a prime implicant from.
+ * @version 1.6.0
+ * @since 1.6.0
+ */
+public class PrimeImplicantGeneration {
+
+    private static final MiniSatPrimeImplicant solver = new MiniSatPrimeImplicant();
+
+    /**
+     * Private constructor.
+     */
+    private PrimeImplicantGeneration() {
+        // Intentionally left empty.
+    }
+
+    /**
+     * Computes any prime implicant from the given formula.
+     * @param formula formula
+     * @return prime implicant or {@code null} if the formula is unsatisfiable
+     */
+    public static SortedSet<Literal> compute(final Formula formula) {
+        solver.reset();
+        return solver.compute(formula);
+    }
+
+    /**
+     * Computes any prime implicant from the given formula on basis of the given implicant.
+     * The resulting prime implicant will therefore consist of a subset (or the same set, if the implicant was already
+     * an prime implicant) of the implicant.
+     *
+     * ATTENTION: The method does not check if the given implicant is really an implicant of the formula for performance
+     * reasons. Ensure that you call this method only if "implicant => formula" holds.
+     * @param formula   formula
+     * @param implicant implicant to compute any prime implicant from
+     * @return prime implicant or {@code null} if the formula is unsatisfiable
+     */
+    public static SortedSet<Literal> computeFrom(final Formula formula, final SortedSet<Literal> implicant) {
+        solver.reset();
+        return solver.computeFrom(formula, implicant);
+    }
+
+    private static class MiniSatPrimeImplicant extends MiniSat2Solver {
+
+        private void newVars(final SortedSet<Variable> variables) {
+            for (final Variable var : variables) {
+                final int index = this.newVar(false, true);
+                this.addName(var.name(), index);
+            }
+        }
+
+        private LNGIntVector generateClauseVector(final Formula clause) {
+            final LNGIntVector clauseVec = new LNGIntVector(clause.numberOfOperands());
+            for (final Literal lit : clause.literals()) {
+                int index = this.idxForName(lit.name());
+                if (index == -1) {
+                    index = this.newVar(false, true);
+                    this.addName(lit.name(), index);
+                }
+                final int litNum = lit.phase() ? index * 2 : (index * 2) ^ 1;
+                clauseVec.push(litNum);
+            }
+            return clauseVec;
+        }
+
+        private void add(final Formula formula) {
+            final Formula cnf = formula.cnf();
+            switch (cnf.type()) {
+                case TRUE:
+                    break;
+                case FALSE:
+                case LITERAL:
+                case OR:
+                    this.addClause(generateClauseVector(cnf), null);
+                    break;
+                case AND:
+                    for (final Formula op : cnf) {
+                        this.addClause(generateClauseVector(op), null);
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("Unexpected formula type in CNF: " + cnf.type());
+            }
+        }
+
+        private List<Integer> getRelevantVarIndices(final Collection<Variable> variables) {
+            final List<Integer> relevantVarIndices = new ArrayList<>(variables.size());
+            for (final Variable var : variables) {
+                final Integer idx = this.name2idx.get(var.name());
+                if (idx != null) {
+                    relevantVarIndices.add(idx);
+                }
+            }
+            return relevantVarIndices;
+        }
+
+        private LNGIntVector toLNGIntVector(final SortedSet<Integer> literals) {
+            final LNGIntVector vector = new LNGIntVector(literals.size());
+            for (final Integer lit : literals) {
+                vector.push(lit);
+            }
+            return vector;
+        }
+
+        private SortedSet<Integer> compute(final SortedSet<Integer> implicant, final SortedSet<Integer> dontCareCandidates) {
+            final SortedSet<Integer> primeImplicant = new TreeSet<>(implicant);
+            for (final Integer lit : dontCareCandidates) {
+                primeImplicant.remove(lit);
+                final LNGIntVector assumptions = toLNGIntVector(primeImplicant);
+                assumptions.push(not(lit));
+                if (solve(null, assumptions) == Tristate.TRUE) {
+                    primeImplicant.add(lit);
+                }
+            }
+            return primeImplicant;
+        }
+
+        private SortedSet<Literal> toLiterals(final FormulaFactory f, final SortedSet<Integer> intLiterals) {
+            final SortedSet<Literal> literals = new TreeSet<>();
+            for (final Integer intLit : intLiterals) {
+                final Integer var = var(intLit);
+                literals.add(f.literal(this.idx2name.get(var), !sign(intLit)));
+            }
+            return literals;
+        }
+
+        SortedSet<Literal> compute(final Formula formula) {
+            final SortedSet<Variable> variables = formula.variables();
+            // Note: Make formula variables known to solver in advance, otherwise the name-index making gets lost when loading previous solver state
+            newVars(variables);
+            final int[] state = saveState();
+            add(formula);
+            if (solve(null) == Tristate.TRUE) {
+                final SortedSet<Integer> implicant = new TreeSet<>();
+                final SortedSet<Integer> dontCareCandidates = new TreeSet<>();
+                for (final Integer var : getRelevantVarIndices(variables)) {
+                    final int lit = mkLit(var, !this.model.get(var));
+                    final MSVariable msVariable = this.vars.get(var);
+                    if (msVariable.level() > 0) {
+                        dontCareCandidates.add(lit);
+                    }
+                    implicant.add(lit);
+                }
+                loadState(state);
+                add(formula.negate());
+                return toLiterals(formula.factory(), compute(implicant, dontCareCandidates));
+            } else {
+                return null;
+            }
+        }
+
+        SortedSet<Literal> computeFrom(final Formula formula, final SortedSet<Literal> implicant) {
+            add(formula.negate());
+            final SortedSet<Integer> intImplicant = new TreeSet<>();
+            for (final Literal lit : implicant) {
+                final Integer idx = this.name2idx.get(lit.name());
+                if (idx != null) {
+                    final int intLit = mkLit(idx, !lit.phase());
+                    intImplicant.add(intLit);
+                }
+            }
+            return toLiterals(formula.factory(), compute(intImplicant, intImplicant));
+        }
+    }
+}
---
+++ src/test/java/org/logicng/primeimplicant/PrimeImplicantGenerationTest.java
@@ -0,0 +1,142 @@
+package org.logicng.primeimplicant;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.logicng.datastructures.Tristate;
+import org.logicng.formulas.Formula;
+import org.logicng.formulas.FormulaFactory;
+import org.logicng.formulas.Literal;
+import org.logicng.io.parsers.ParserException;
+import org.logicng.io.parsers.PropositionalParser;
+import org.logicng.io.readers.DimacsReader;
+import org.logicng.io.readers.FormulaReader;
+import org.logicng.predicates.satisfiability.SATPredicate;
+import org.logicng.predicates.satisfiability.TautologyPredicate;
+import org.logicng.solvers.MiniSat;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class PrimeImplicantGenerationTest {
+
+    private static final FormulaFactory f = new FormulaFactory();
+    private static final PropositionalParser p = new PropositionalParser(f);
+
+    private void testImplicantProperty(final SortedSet<Literal> implicant, final Formula formula) {
+        assertThat(f.and(implicant).holds(new SATPredicate(f))).isTrue();
+        assertThat(f.implication(f.and(implicant), formula).holds(new TautologyPredicate(f))).isTrue();
+    }
+
+    private void testMinimalityProperty(final SortedSet<Literal> primeImplicant, final Formula formula) {
+        final MiniSat solver = MiniSat.miniSat(f);
+        solver.add(formula.negate());
+        for (final Literal lit : primeImplicant) {
+            final SortedSet<Literal> assumptions = new TreeSet<>(primeImplicant);
+            assumptions.remove(lit);
+            assumptions.add(lit.negate());
+            assertThat(solver.sat(assumptions)).isEqualTo(Tristate.TRUE);
+        }
+    }
+
+    private void testAnyPrimeImplicant(final Formula formula) {
+        final long start = System.currentTimeMillis();
+        final SortedSet<Literal> primeImplicant = PrimeImplicantGeneration.compute(formula);
+        final long end = System.currentTimeMillis();
+        System.out.println("running time (ms): " + (end - start));
+        System.out.println(primeImplicant);
+        if (formula.holds(new SATPredicate(f))) {
+            assertThat(primeImplicant).isNotNull();
+            testImplicantProperty(primeImplicant, formula);
+            testMinimalityProperty(primeImplicant, formula);
+        } else {
+            assertThat(primeImplicant).isNull();
+        }
+
+    }
+
+    private Formula getBlockingClause(final SortedSet<Literal> literals) {
+        final SortedSet<Literal> negatedLiterals = new TreeSet<>();
+        for (final Literal lit : literals) {
+            negatedLiterals.add(lit.negate());
+        }
+        return f.or(negatedLiterals);
+    }
+
+    private void testPrimeImplicantFrom(final Formula formula, final int numPrimeImplicants) {
+        final MiniSat satSolver = MiniSat.miniSat(formula.factory());
+        satSolver.add(formula);
+        boolean runOutOfModels = false;
+        final List<SortedSet<Literal>> primeImplicants = new ArrayList<>();
+        for (int i = 0; i < numPrimeImplicants; ++i) {
+            final boolean sat = satSolver.sat() == Tristate.TRUE;
+            if (sat) {
+                final SortedSet<Literal> implicant = satSolver.model(formula.variables()).literals();
+                final long start = System.currentTimeMillis();
+                final SortedSet<Literal> primeImplicant = PrimeImplicantGeneration.computeFrom(formula, implicant);
+                final long end = System.currentTimeMillis();
+                System.out.println("running time (ms): " + (end - start));
+                System.out.println(primeImplicant);
+
+                primeImplicants.add(primeImplicant);
+                testImplicantProperty(primeImplicant, formula);
+                testMinimalityProperty(primeImplicant, formula);
+                satSolver.add(getBlockingClause(primeImplicant));
+            } else {
+                runOutOfModels = true;
+                break;
+            }
+        }
+        if (runOutOfModels) {
+            Formula cover = f.falsum();
+            for (final SortedSet<Literal> primeImplicant : primeImplicants) {
+                cover = f.or(cover, f.and(primeImplicant));
+            }
+            assertThat(f.equivalence(cover, formula).holds(new TautologyPredicate(f))).isTrue();
+        }
+    }
+
+    private void test(final Formula formula, final int numPrimeImplicants) {
+        testAnyPrimeImplicant(formula);
+        testPrimeImplicantFrom(formula, numPrimeImplicants);
+    }
+
+    @Test
+    public void testSimple() throws ParserException {
+        test(p.parse("a & b & c"), 5);
+        test(p.parse("a | b | c"), 5);
+        test(p.parse("a & (b | c)"), 5);
+        test(p.parse("a & (b | c) & c & (D | E) & D"), 5);
+    }
+
+    @Test
+    public void testSmallFormulas() throws IOException, ParserException {
+        final Formula formula = FormulaReader.readPseudoBooleanFormula("src/test/resources/formulas/small_formulas.txt", f);
+        test(formula, 5);
+    }
+
+    @Test
+    public void testLargeFormula() throws IOException, ParserException {
+        final Formula formula = FormulaReader.readPseudoBooleanFormula("src/test/resources/formulas/large_formula.txt", f);
+        test(formula, 5);
+    }
+
+    @Ignore
+    @Test
+    public void testDimacsFiles() throws IOException {
+        final File testFolder = new File("src/test/resources/sat");
+        final File[] files = testFolder.listFiles();
+        assert files != null;
+        for (final File file : files) {
+            final String fileName = file.getName();
+            if (fileName.endsWith(".cnf")) {
+                test(f.cnf(DimacsReader.readCNF(file, f)), 2);
+            }
+        }
+    }
+}
